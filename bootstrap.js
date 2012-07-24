a4396ff957751b95dd13e90959ee4ffc662e4386
meh <meh@paranoici.org>
2012-07-24 14:58:47 +0200

    Preserve user changes.

--- bootstrap.js
+++ bootstrap.js
@@ -12,46 +12,42 @@ const { classes: Cc, interfaces: Ci, results: Cr, utils: Cu } = Components;
 
 var Blender = (function () {
 	var c = function () {};
-	var preferences = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("general.");
+
+	var Preferences = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("general.");
+	var Settings    = {
+		"appname.override":    "Netscape",
+		"appversion.override": "5.0 (Windows)",
+		"buldID.override":     "0",
+		"oscpu.override":      "Windows NT 6.1",
+		"platform.override":   "Win32",
+		"productSub.override": "20100101",
+
+		"useragent.override":  "Mozilla/5.0 (Windows NT 6.1; rv:10.0) Gecko/20100101 Firefox/10.0",
+		"useragent.vendor":    "",
+		"useragent.vendorSub": "",
+
+		"navigator.platform":     "Win32",
+		"navigator.system":       "Windows NT 6.1",
+		"navigator.appVersion":   "5.0 (Windows)",
+		"navigator.buildID":      "20100101",
+		"navigator.geckoVersion": "10.0",
+		"navigator.version":      "10.0"
+	}
 
 	c.prototype.start = function () {
-		preferences.setCharPref("appname.override", "Netscape");
-		preferences.setCharPref("appversion.override", "5.0 (Windows)");
-		preferences.setCharPref("buldID.override", "0");
-		preferences.setCharPref("oscpu.override", "Windows NT 6.1");
-		preferences.setCharPref("platform.override", "Win32");
-		preferences.setCharPref("productSub.override", "20100101");
-
-		preferences.setCharPref("useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:10.0) Gecko/20100101 Firefox/10.0");
-		preferences.setCharPref("useragent.vendor", "");
-		preferences.setCharPref("useragent.vendorSub", "");
-
-		preferences.setCharPref("navigator.platform", "Win32");
-		preferences.setCharPref("navigator.system", "Windows NT 6.1");
-		preferences.setCharPref("navigator.appVersion", "5.0 (Windows)");
-		preferences.setCharPref("navigator.buildID", "20100101");
-		preferences.setCharPref("navigator.geckoVersion", "10.0");
-		preferences.setCharPref("navigator.version", "10.0");
+		for (var name in Settings) {
+			if (!Preferences.prefHasUserValue(name)) {
+				Preferences.setCharPref(name, Settings[name]);
+			}
+		}
 	}
 
 	c.prototype.stop = function () {
-		preferences.clearUserPref("appname.override");
-		preferences.clearUserPref("appversion.override");
-		preferences.clearUserPref("buldID.override");
-		preferences.clearUserPref("oscpu.override");
-		preferences.clearUserPref("platform.override");
-		preferences.clearUserPref("productSub.override");
-
-		preferences.clearUserPref("useragent.override");
-		preferences.clearUserPref("useragent.vendor");
-		preferences.clearUserPref("useragent.vendorSub");
-
-		preferences.clearUserPref("navigator.platform");
-		preferences.clearUserPref("navigator.system");
-		preferences.clearUserPref("navigator.appVersion");
-		preferences.clearUserPref("navigator.buildID");
-		preferences.clearUserPref("navigator.geckoVersion");
-		preferences.clearUserPref("navigator.version");
+		for (var name in Settings) {
+			if (Preferences.getCharPref(name) === Settings[name]) {
+				Preferences.clearUserPref(name);
+			}
+		}
 	}
 
 	return c;
